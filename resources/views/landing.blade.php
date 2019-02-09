e40c1e3b85d30ff16af9df8cdc37c4bf710fe358
Clint Winter <clintw@cwrelectronics.com>
2019-02-08 19:28:30 -0500

    Getting situated

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/css/app.css">
+
+    <title>Wardrobe Manager</title>
+</head>
+<body>
+
+    <h1>WARDROBE MANAGER</h1>
+
+    <p>This will be a beautiful project one day.</p>
+    
+</body>
+</html>
